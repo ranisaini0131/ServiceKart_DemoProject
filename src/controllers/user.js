61e05d7059aea156d70ddda1d0554abe2c4f5e8c
Rani Saini <sainirani1797@gmail.com>
2024-08-02 16:08:15 +0530

    verify_number api

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,7 +1,7 @@
 import { qb } from "../../server.js"
 import jwt from "jsonwebtoken"
 
-export const signup = async (req, res) => {
+export const check_phone_number = async (req, res) => {
     try {
         //get user details from frontend
         const { phone_number } = req.body
@@ -29,9 +29,50 @@ export const signup = async (req, res) => {
                 });
             } else {
                 console.log('User does not exist');
+                return res.status(409).json({
+                    status: "failed",
+                    message: "User does not exists"
+                });
+            }
+        })
+
+    } catch (error) {
+        console.log("error:", error)
+    }
+}
+
+
+export const signup = async (req, res) => {
+    try {
+        //get user details from frontend
+        const { phone_number, first_name, last_name, DOB, email, gender } = req.body
+
+        //validation
+        if (!(first_name || last_name || DOB || gender || email)) {
+            return res.status(400).json({
+                status: "failed",
+                message: "Please provide all fields",
+            })
+        }
+
+
+        //check if user already exists or not
+
+        const existedUser = `SELECT id FROM users WHERE phone_number = ${phone_number}`
+
+        await qb.query(existedUser, async (err, results) => {
+            if (err) throw err;
+
+            if (results.length > 0) {
+                return res.status(409).json({
+                    status: "failed",
+                    message: "User already exists"
+                });
+            } else {
+                console.log('User does not exist');
 
                 //create new user
-                const newUser = await qb.query(`INSERT INTO users (phone_number) VALUES ('${phone_number}')`)
+                const newUser = await qb.query(`INSERT INTO users (phone_number ,first_name,last_name , DOB,gender,email) VALUES ('${phone_number}','${first_name}','${last_name}','${DOB}', '${gender}', '${email}')`)
 
                 if (!newUser) {
                     return res.status(500).json({
@@ -58,21 +99,19 @@ export const signup = async (req, res) => {
 
 export const login = async (req, res) => {
     try {
-        const { phone_number, first_name, last_name, DOB, email, gender } = req.body
-        console.log(req.body)
+        const { phone_number } = req.body
 
         //check fields
-        if (!(first_name || last_name || DOB || gender || email)) {
+        if (!phone_number) {
             return res.status(422).json({
                 status: "fail",
-                message: "Please provide username or email"
+                message: "Please provide required field"
             })
         }
 
         //check existed user
-        const existedUser = `SELECT id FROM users WHERE (phone_number ) = ${phone_number}`
+        const existedUser = `SELECT id FROM users WHERE phone_number = ${phone_number}`
 
-        console.log(req.body.id, "73")
 
 
         await qb.query(existedUser, async (err, results) => {
@@ -83,7 +122,7 @@ export const login = async (req, res) => {
                 //generate token
                 const token = jwt.sign(
                     {
-                        phone_number
+                        id: results[0].id,
                     },
                     process.env.SECREAT_KEY,
                     {
@@ -91,13 +130,17 @@ export const login = async (req, res) => {
                     }
                 )
 
+                if (!token) {
+                    return res.status(401).json({ message: 'Authentication failed' });
+                }
+
                 //return response 
                 return res
                     .status(200)
                     .json({
                         status: 'success',
                         data: token,
-                        message: "User Login Successfully"
+                        message: "User Successfully"
                     })
             } else {
                 console.log('User does not exist');
@@ -115,3 +158,20 @@ export const login = async (req, res) => {
         console.log("Error: ", error.message)
     }
 }
+
+
+
+// //generate token
+// const token = jwt.sign(
+//     {
+//         id: results[0].id
+//     },
+//     process.env.SECREAT_KEY,
+//     {
+//         expiresIn: process.env.TOKEN_EXPIRY
+//     }
+// )
+
+// if (!token) {
+//     return res.status(401).json({ message: 'Authentication failed' });
+// }
--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -1,10 +1,10 @@
 import { Router } from "express";
-import { login, signup } from "../controllers/user.js";
+import { login, signup, check_phone_number } from "../controllers/user.js";
 
 const router = Router()
 
 
-
+router.post("/check_phone_number", check_phone_number)
 router.post("/signup", signup)
 router.post("/login", login)
 
